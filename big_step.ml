796756acb6380561f9dbac0f2e4dc5befa6a56bc
gabrielbp <gabriel_ciac6de@hotmail.com>
2019-06-27 15:15:01 -0300

    commit inicial.

---
+++ big_step.ml
@@ -0,0 +1,4 @@
+#use "sintaxe.ml"
+#use "resultados.ml"
+
+let rec evaluate (envmnt : env) (e : expr) = ()
---
+++ sintaxe.ml
@@ -0,0 +1,35 @@
+type variable = string
+
+type bop = Sum   (* + *)
+         | Sub (* - *)
+         | Mult  (* x *)
+         | Div   (* / *)
+         | And   (* and *)
+         | Or    (* or *)
+         | Eq    (* = *)
+         | Df    (* <> *)
+         | Lt    (*  < *)
+         | Le    (* <= *)
+         | Gt    (* > *)
+         | Ge    (* >= *)
+ 
+type uop = Not
+
+type expr = Ncte of int 
+          | Bcte of bool
+          | Binop of bop * expr * expr
+          | Unop of uop * expr
+          | Pair of expr * expr  
+          | If of expr * expr * expr 
+          | Var of variable 
+          | App of expr * expr 
+          | Lam of variable * expr
+          | Let of variable * expr * expr
+          | Lrec of variable *  variable * expr * expr
+          | Nil
+          | Cons of expr * expr
+          | IsEmpty of expr
+          | Hd of expr
+          | Tl of expr
+          | Raise
+          | Try of expr * expr
